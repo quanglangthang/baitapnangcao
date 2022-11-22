56dd11c7e94a4b4e7898970179faac9a2fcfe8c7
Quang lang thang <63640626+quanglangthang@users.noreply.github.com>
2022-11-22 16:51:26 +0700

    Update Bai3.cpp

--- Bai3.cpp
+++ Bai3.cpp
@@ -2,31 +2,38 @@
 using namespace std;
 
 int main() {
-	int month;
-	cout << "Nhap thang can xem ngay: ";
-	cin >> month;
-	
-	if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12)) {
-		cout << "Co 31 ngay";
-	}
-	else if ((month == 4) || (month == 6) || (month == 9) || (month == 1)) {
-		cout << "Co 30 ngay";
-	}
-	else {
-		int year;
-		cout << "Nhap nam cua cac thang can xem: ";
-		cin >> year;
+	int month, x = 1;
+	do {
+		cout << "Nhap thang can xem ngay: ";
+		cin >> month;
+		switch (month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			cout << "Co 31 ngay";
+			break;
+		case 4: case 6: case 9: case 11:
+			cout << "Co 30 ngay";
+			break;
+		case 2:
+			int year;
+			cout << "Nhap nam cua cac thang can xem: ";
+			cin >> year;
 
-		if (year % 400 == 0) {
-			cout << "Co 29 ngay ";
-		}
-		else {
-			if ((year % 4 == 0) && (year % 100 != 0)) {
-				cout << "Co 29 ngay";
+			if (year % 400 == 0) {
+				cout << "Co 29 ngay ";
 			}
 			else {
-				cout << "Co 28 ngay";
+				if ((year % 4 == 0) && (year % 100 != 0)) {
+					cout << "Co 29 ngay";
+				}
+				else {
+					cout << "Co 28 ngay";
+				}
 			}
+			break;
+		default:
+			x = 0;
 		}
-	}
+	} while (x == 0);
+	
+	
 }
